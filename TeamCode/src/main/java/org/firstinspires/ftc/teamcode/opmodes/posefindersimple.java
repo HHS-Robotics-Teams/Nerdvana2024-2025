639774bb33266e929b95e6a64460410b9815a63e
JoshR <joshroun@gmail.com>
2024-10-20 20:07:43 -0400

    pose finder opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/posefindersimple.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp (name = "posefindersimple",group ="testing" )
+public class posefindersimple extends OpMode {
+
+    DcMotor pivot_motor;
+    Servo pivot_Servo;
+    Servo  extendo_servo;
+
+
+    @Override
+    public void init() {
+        pivot_Servo = hardwareMap.get(Servo.class, "rodo");
+        pivot_motor =hardwareMap.get(DcMotor.class,"pivot");
+        extendo_servo= hardwareMap.get(Servo.class,"extendoarm");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("rodo position",pivot_Servo.getPosition());
+        telemetry.addData("pivot position",pivot_motor.getCurrentPosition());
+        telemetry.addData("extendo position",extendo_servo.getPosition());
+    }
+}
